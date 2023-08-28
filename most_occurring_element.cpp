b1fe96fd87ef44f403f526f73687782f36358b47
Arjun Poduval <arjunmpoduval@gmail.com>
2023-08-28 22:37:08 +0530

    most occurring element update

--- most_occurring_element.cpp
+++ most_occurring_element.cpp
@@ -4,26 +4,29 @@ using namespace std;
 
 void most_occurring_element(int arr[], int n){
 
-    for(int i=0; i < n; i++){
+    int prev=0,element=0;
+    for(int i=0; i < n; i++)
+    {
         int count = 0;
-        int s = 0;
         for(int j=0; j < n; j++)
         {
             if(arr[i] == arr[j])
-                count++;                
-        }
-        
-        
+                count++;  
             
+        }//for j
 
-    }
+        if(count>prev){         
+            prev = count;   
+            element = arr[i];
+        }
 
+    }//for i
+    cout <<"most occurring element: "<< element << " ";
 }
 
 int main(){
-    int num[] =  {4,23,3,4,66,3,5,4,56,23,25};
+    int num[] = {2,5,3,2,5,5,7,87,4,5};
     int n = sizeof(num)/sizeof(num[0]);
     most_occurring_element(num, n);
-
     return 0;
 }
